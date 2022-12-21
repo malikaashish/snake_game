e391dd61fb727c1d9c8b3fd4929d64b5be1ab3ab
Aashish Malik <aashishmalik3105@gmail.com>
2022-12-21 23:37:08 +0530

    Initial commit

---
+++ snake.py
@@ -0,0 +1,54 @@
+from turtle import  Turtle
+STARTING_POSITIONS =[(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            self.add_segment(pos)
+
+    def add_segment(self,pos):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(pos)
+        self.segments.append(segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[seg_num - 1].xcor()
+            y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].setposition(x, y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
